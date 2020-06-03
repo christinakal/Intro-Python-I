9baa62bb6601c4b6f808b9cb35ceb67f39f4b3d0
Christina Kaliora <crhstina.kaliora@pirron.one>
2020-06-03 09:40:26 +0200

    finished all excersises but I'm getting weird errors on 15

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,9 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(x, y):
+    return x + y
+
 
 print(f1(1, 2))
 
@@ -13,6 +16,8 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    return sum(args)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -20,6 +25,9 @@ print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
+
+def f2(*args):
+    return sum(*args)
 
 # How do you have to modify the f2 call below to make this work?
 print(f2(a))    # Should print 22
@@ -30,6 +38,12 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(*args):
+    if len(args) == 2:
+        return sum(args)
+    elif len(args) == 1:
+        return args[0] + 1
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +58,10 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for key,value in kwargs.items():
+        print(f'key: {key}, value: {value}')
+
 
 # Should print
 # key: a, value: 12
@@ -60,6 +78,11 @@ d = {
     "monster": "goblin",
     "hp": 3
 }
+
+def f4(*kwargs):
+    for val in kwargs:
+       for key,value in val.items():
+        print(f'key: {key}, value: {value}')
 
 # How do you have to modify the f4 call below to make this work?
 f4(d)
